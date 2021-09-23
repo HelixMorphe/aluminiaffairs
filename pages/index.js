368fc291d56b1bdd587bc79609bd6976dde9dc28
santhosh-cloud <56725056+santhosh-cloud@users.noreply.github.com>
2021-09-23 10:32:50 +0530

    title change

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Background from "../components/Background";
+import Head from 'next/head'
 const index = () => {
   return (
     <div>
       <Header />
       <Background />
+    <Head>
+        <title>5th Alumni Meet 2021</title>
+    </Head>
     </div>
   );
 };
